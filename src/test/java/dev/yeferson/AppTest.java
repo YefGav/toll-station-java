b6cbdcdca9b1672660e3ba83f75ea6810557173d
unknown <gaviriayeferson2@gmail.com>
2025-03-31 20:41:01 +0200

    Add unit tests for App using ByteArray streams

--- src/test/java/dev/yeferson/AppTest.java
+++ src/test/java/dev/yeferson/AppTest.java
@@ -1,18 +1,40 @@
 package dev.yeferson;
 
 import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
-/**
- * Unit test for simple App.
- */
-class AppTest {
-    /**
-     * Rigorous Test.
-     */
+public class AppTest {
+
     @Test
-    void testApp() {
-        assertEquals(1, 1);
+    public void testAppMainWithCar() {
+  
+        String simulatedInput = "1\nABC123\n";
+        ByteArrayInputStream testInput = new ByteArrayInputStream(simulatedInput.getBytes());
+        System.setIn(testInput);
+
+       
+        ByteArrayOutputStream testOutput = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(testOutput));
+
+        try {
+    
+            App.main(new String[]{});
+        } finally {
+   
+            System.setIn(System.in);
+            System.setOut(originalOut);
+        }
+
+        String output = testOutput.toString();
+        
+        assertThat(output, containsString("Toll Station: Huerna in Asturias"));
+        assertThat(output, containsString("ABC123"));
+        assertThat(output, containsString("Total Collected: $100.0"));
     }
 }

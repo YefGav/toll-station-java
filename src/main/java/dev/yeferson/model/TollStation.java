62953fe63da356be30a750b4d8ef69b0c2f0b45a
unknown <gaviriayeferson2@gmail.com>
2025-03-31 20:15:56 +0200

    Implement registerVehicle in TollStation

--- src/main/java/dev/yeferson/model/TollStation.java
+++ src/main/java/dev/yeferson/model/TollStation.java
@@ -1,15 +1,19 @@
 package dev.yeferson.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TollStation {
 
     private String name;
     private String city;
     private double totalCollected = 0.0;
-    
+    private List<Vehicle> vehicles = new ArrayList<>();
+
     public TollStation(String name, String city) {
         this.name = name;
         this.city = city;
-        
+
     }
 
     public String getName() {
@@ -24,6 +28,9 @@ public class TollStation {
         return totalCollected;
     }
 
-    
+    public void registerVehicle(Vehicle vehicle) {
+        totalCollected += vehicle.getTollValue();
+        vehicles.add(vehicle);
+    }
 
 }
